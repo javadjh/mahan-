db8f74cd9c24d3ff71e38930b6b0384c5ced667c
javad <javad>
2022-12-21 18:25:33 +0330

    ip

--- utility/OCR.js
+++ utility/OCR.js
@@ -1,9 +1,9 @@
 const Tesseract = require("tesseract.js");
 module.exports.OCR = async (file) => {
   console.log(file);
-  console.log("http://192.168.2.24:5000/" + file);
+  console.log("http://87.236.215.49:5000/" + file);
   const data = await Tesseract.recognize(
-    "http://192.168.2.24:5000/" + file,
+    "http://87.236.215.49:5000/" + file,
     "fas",
     { logger: (m) => console.log(m) }
   );
--- web/src/config/ip.js
+++ web/src/config/ip.js
@@ -1,2 +1,2 @@
-export const FRONT_IP = "http://192.168.2.24:3000";
-export const SERVER_IP = "http://192.168.2.24:5000";
+export const FRONT_IP = "http://87.236.215.49:80";
+export const SERVER_IP = "http://87.236.215.49:5000";
