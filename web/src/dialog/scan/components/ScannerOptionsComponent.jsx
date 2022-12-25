35eca27cbfcc6303a44e4965855faa5420f7d677
javad <javad>
2022-12-25 14:49:22 +0330

    ip

--- web/src/dialog/scan/components/ScannerOptionsComponent.jsx
+++ web/src/dialog/scan/components/ScannerOptionsComponent.jsx
@@ -1,65 +1,85 @@
-import { Button, Col, Form, Image, Input, Radio, Row, Select, Slider } from 'antd';
-import React from 'react';
-const ScannerOptionsComponent = ({devices,file,onScannedListener})=>{
-    return (
-        <div className="flex-second">
-        <div style={{padding:30}}>
-          <div>انتخاب دستگاه</div>
-          <Form.Item name={"deviceId"}>
-            <Select style={{width:"100%"}}>
-              {devices.map(device=>(
-                <Select.Option key={device.DeviceId} value={device.DeviceId}>{device.DeviceName}</Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <div>رزولوشن</div>
-          <Form.Item name={"dpi"}>
-            <Select style={{width:"100%"}}>
-              <Select.Option>950dpi</Select.Option>
-            </Select>
-          </Form.Item>
-          
-          <label>ابعاد</label>
-          <Row>
-            <Col span={12}>
+import {
+  Button,
+  Col,
+  Form,
+  Image,
+  Input,
+  Radio,
+  Row,
+  Select,
+  Slider,
+} from "antd";
+import React from "react";
+const ScannerOptionsComponent = ({ devices, file, onScannedListener }) => {
+  return (
+    <div className="flex-second">
+      <div style={{ padding: 30 }}>
+        <div>انتخاب دستگاه</div>
+        <Form.Item name={"deviceId"}>
+          <Select style={{ width: "100%" }}>
+            {devices.map((device) => (
+              <Select.Option key={device.Id} value={device.Id}>
+                {device.Name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <div>رزولوشن</div>
+        <Form.Item name={"dpi"}>
+          <Select style={{ width: "100%" }}>
+            <Select.Option>950dpi</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <label>ابعاد</label>
+        <Row>
+          <Col span={12}>
             <Form.Item name={"width_pixels"}>
-              <Input placeholder='طول را وارد کنید...'/>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
+              <Input placeholder="طول را وارد کنید..." />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
             <Form.Item name={"height_pixels"}>
-              <Input placeholder='عرض را وارد کنید...'/>
-              </Form.Item>
+              <Input placeholder="عرض را وارد کنید..." />
+            </Form.Item>
+          </Col>
+        </Row>
+        <label>brightness</label>
+        <Form.Item name={"brightnessPercents"}>
+          <Slider defaultValue={0.5} max={1} step={0.01} />
+        </Form.Item>
+        <label>contrast</label>
+        <Form.Item name={"contrastPercents"}>
+          <Slider defaultValue={0.5} max={1} step={0.01} />
+        </Form.Item>
+        <Form.Item name={"colorMode"}>
+          <Radio.Group>
+            <Radio value={1}>رنگی</Radio>
+            <Radio value={0}>سیاه سفید</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <div style={{ marginTop: 50 }}>
+          <Row>
+            <Col span={11}>
+              <Button htmlType="submit" block>
+                اسکن
+              </Button>
+            </Col>
+            <Col hidden={!file} span={12} offset={1}>
+              <Button
+                onClick={() => {
+                  onScannedListener(file);
+                }}
+                block
+                type="primary"
+              >
+                ارسال
+              </Button>
             </Col>
           </Row>
-          <label>brightness</label>
-          <Form.Item name={"brightnessPercents"}>
-            <Slider defaultValue={0.5} max={1} step={0.01}/>
-          </Form.Item>
-          <label>contrast</label>
-          <Form.Item name={"contrastPercents"}>
-            <Slider defaultValue={0.5} max={1} step={0.01}/>
-          </Form.Item>
-          <Form.Item name={"colorMode"}>
-            <Radio.Group >
-              <Radio value={1}>رنگی</Radio>
-              <Radio value={0}>سیاه سفید</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <div style={{marginTop:50}}>
-            <Row>
-              <Col span={11}>
-                <Button htmlType='submit' block>اسکن</Button>
-              </Col>
-              <Col hidden={!file} span={12} offset={1}>
-                <Button onClick={()=>{
-                  onScannedListener(file)
-                }} block type='primary'>ارسال</Button>
-              </Col>
-            </Row>
-          </div>
-          </div>
+        </div>
       </div>
-    )
-}
-export default ScannerOptionsComponent
+    </div>
+  );
+};
+export default ScannerOptionsComponent;
