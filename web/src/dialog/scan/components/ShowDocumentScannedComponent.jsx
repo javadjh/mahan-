7c3885cb1c592563fb40c6e3899d8575ea414861
javad <javad>
2023-01-09 18:07:21 +0330

    fix

--- web/src/dialog/scan/components/ShowDocumentScannedComponent.jsx
+++ web/src/dialog/scan/components/ShowDocumentScannedComponent.jsx
@@ -1,19 +1,23 @@
-import { Image } from "antd"
-import React from 'react';
-const ShowDocumentScannedComponent = ({isScanning,blobImage})=>{
-    return (
-        <>
-            <div className="flex-first">
-                {isScanning ?(
-                <div style={{paddingTop:"35%",color:"white"}}>
-                    در حال اسکن...
-                </div>
-                ):(
-                <Image src={blobImage} height={"100vh"} preview={false}/>
-                )}
-                
-            </div>
-        </>
-    )
-}
-export default ShowDocumentScannedComponent
+import { Image } from "antd";
+import React from "react";
+const ShowDocumentScannedComponent = ({ isScanning, blobImage }) => {
+  return (
+    <>
+      <div className="flex-first">
+        {isScanning ? (
+          <div style={{ paddingTop: "35%", color: "white" }}>
+            در حال اسکن...
+          </div>
+        ) : (
+          <Image
+            width={"100%"}
+            src={blobImage}
+            height={"100vh"}
+            preview={false}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+export default ShowDocumentScannedComponent;
